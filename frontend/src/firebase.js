1920c1e3ceaf7d3170efbb606bd34d50e6a0e522
shivam shukla <264753159+shivamshukla02@users.noreply.github.com>
2026-09-03 10:22:44 +0530

    add google login using env vars

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmMYjwvl8JeEg6IOQ56ZDwGb3vToGgzKA",
-  authDomain: "syntrix-ic.firebaseapp.com",
-  projectId: "syntrix-ic",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: "syntrix-ic.firebasestorage.app",
   messagingSenderId: "1054966153898",
   appId: "1:1054966153898:web:462d134046221b2ece86d3",
